a8af778d20de0202fc72cb394835452891aa2e5f
Kim <kimniklas.schlueter@gmail.com>
2025-01-02 20:54:07 +0100

    cmake project restructure

--- build_windows.bat
+++ build_windows.bat
@@ -1,2 +1,2 @@
-cmake -S . -B ./build -G "Visual Studio 17 2022" -DCMAKE_TOOLCHAIN_FILE=toolchain_windows.cmake
+cmake -S . -B ./build -G "Visual Studio 17 2022" -DCMAKE_TOOLCHAIN_FILE=toolchain_windows.cmake -T v143
 PAUSE
